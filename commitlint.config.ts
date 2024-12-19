7142807415596d558630bef92570e386054bbfaa
Gabriele Canepa <git@gabrielecanepa.com>
2024-12-19 16:52:02 +0100

    ci: ignore dependency updates in commitlint

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,6 +1,9 @@
 import { type UserConfig } from '@commitlint/types'
 
+const DEPENDENCY_UPDATES = /^build\((deps|deps-dev|security)\):/
+
 export default {
   defaultIgnores: true,
   extends: ['@commitlint/config-conventional'],
+  ignores: [message => DEPENDENCY_UPDATES.test(message)],
 } satisfies UserConfig
